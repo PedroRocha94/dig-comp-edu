c658215b91d47da3962bdf33104b6e42e72c3122
Pedro Rocha <pedrorocha73658@gmail.com>
2022-09-26 10:19:18 -0300

    Creating project.

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Bem vindo</h1>
+    <span>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Exercitationem, ipsam accusantium repellat aliquam quae odio sapiente 
+      illo pariatur hic ut nostrum illum, impedit harum nemo ex eligendi 
+      praesentium corporis. Aliquam!
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
