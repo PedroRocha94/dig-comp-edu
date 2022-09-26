02f2a334e9ed673ca388594b5ab2a58ead96c853
Pedro Rocha <pedrorocha73658@gmail.com>
2022-09-26 18:06:36 -0300

    Creating Quiz page and adding initial SCSS.

---
+++ src/pages/Quiz.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="quiz">
+    <Sidebar/>
+    <div class="content">
+      <h1>Questionário</h1>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Incidunt architecto saepe iste eaque temporibus. Error accusamus, 
+        sunt ullam ipsum ad atque laborum quo pariatur vel fuga dolor, culpa 
+        dolores quidem!
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
+import Quiz from '../pages/Quiz.vue';
 import About from '../pages/About.vue';
 
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/quiz',
+    name: 'quiz',
+    component: Quiz
   },
   {
     path: '/about',
