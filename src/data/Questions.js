027125f7623ee11228a0f5dbd84bac30fa8a7996
Pedro Rocha <pedrorocha73658@gmail.com>
2022-10-14 12:11:23 -0300

    Adding the ChartJs library to the project, and inserting the questions into the question file.

--- src/data/Questions.js
+++ src/data/Questions.js
@@ -112,7 +112,247 @@ export const questions = ref([
       'Divulgo dados pessoais de forma abrangente; p. ex. combinando senhas difíceis de adivinhar com encriptação e atualizações frequentes de software'
     ],
     selected: null
-  }
+  },
+  {
+    id: 1,
+    area: {
+      id: 3,
+      name: 'Ensino e Aprendizagem'
+    },
+    question: 'Pondero, cuidadosamente, como, quando e por que usar tecnologias digitais em sala de aula, para garantir que elas sejam usadas com valor acrescentado',
+    options: [
+      'Não uso, ou raramente uso, tecnologia em sala de aula',
+      'Faço uma utilização básica do equipamento disponível; p. ex. quadros interativos ou projetores multimídias',
+      'Uso uma variedade de recursos e ferramentas digitais em minhas aulas',
+      'Uso ferramentas digitais para melhorar sistematicamente minhas aulas',
+      'Uso ferramentas digitais para implementar estratégias pedagógicas inovadoras'
+    ],
+    selected: null
+  },
+  {
+    id: 2,
+    area: {
+      id: 3,
+      name: 'Ensino e Aprendizagem'
+    },
+    question: 'Monitoro as atividade e interações dos meus alunos nos ambientes colaborativos online que usamos',
+    options: [
+      'Não utilizo ambientes digitais com os meus alunos',
+      'Não monitoro a atividade dos alunos nos ambientes online que utilizo',
+      'Ocasionalmente verifico as discussões dos alunos',
+      'Monitoro e analiso a atividade online dos meus alunos regularmente',
+      'Intervenho com comentários motivadores ou corretivos regularmente'
+    ],
+    selected: null
+  },
+  {
+    id: 3,
+    area: {
+      id: 3,
+      name: 'Ensino e Aprendizagem'
+    },
+    question: 'Quando os meus alunos trabalham em grupos, usam tecnologias digitais para adquirir e documentar conhecimento',
+    options: [
+      'Os meus alunos não trabalham em grupos',
+      'Não é possível, para mim, integrar tecnologias digitais em trabalho em grupo',
+      'Incentivo os alunos trabalharem em grupos, procurando informação online ou apresentar os seus resultados em um formato digital',
+      'Peço aos alunos que trabalham em grupos e utilizem a internet para encontrarem informação e apresentarem os seus resultados em um formato digital',
+      'Os meus alunos trocam evidências e criam conhecimento juntos, num espaço colaborativo online'
+    ],
+    selected: null
+  },
+  {
+    id: 4,
+    area: {
+      id: 3,
+      name: 'Ensino e Aprendizagem'
+    },
+    question: 'Uso tecnologias digitais para permitir que os alunos planejem, documentem e monitorem as suas aprendizagens; p. ex. quizzes para autoavaliação, portfólios para documentação e divulgação, diários online/blogues para reflexão...',
+    options: [
+      'Não é possível no meu contexto de trabalho',
+      'Os meus alunos refletem sobre a sua aprendizagem, mas não com tecnologias digitais',
+      'Às vezes uso; p. ex., quizzes para autoavaliação',
+      'Uso uma variedade de ferramentas digitais para permitir aos alunos planejar, documentar ou refletir sobre a sua aprendizagem',
+      'Integro, sistematicamente, diferentes ferramentas digitais para planejar, monitorar e refletir sobre o progresso dos alunos'
+    ],
+    selected: null
+  },
+  {
+    id: 1,
+    area: {
+      id: 4,
+      name: 'Avaliação'
+    },
+    question: 'Uso ferramentas de avaliação digital para monitorar o progresso de aprendizagem dos alunos',
+    options: [
+      'Não monitoro o progresso dos alunos',
+      'Monitoro o progresso regularmente, mas não através de meios digitais',
+      'Às vezes utilizo uma ferramenta digital; p. ex. um quiz, para controlar o progresso dos alunos',
+      'Uso uma variedade de ferramentas digitais para monitorar o progresso dos alunos',
+      'Uso, sistematicamente, uma variedade de ferramentas digitais para monitorar o progresso dos alunos'
+    ],
+    selected: null
+  },
+  {
+    id: 2,
+    area: {
+      id: 4,
+      name: 'Avaliação'
+    },
+    question: 'Analiso todos os dados disponíveis para identificar, efetivamente, os alunos que precisam de apoio adicional. Os “dados” incluem: envolvimento dos alunos, desempenho, classificações, participação; atividades e interações sociais em ambientes (online). “Alunos que precisam de apoio adicional” são: alunos que correm o risco de desistir ou apresentam baixo desempenho; alunos que têm distúrbios de aprendizagem ou necessidades específicas de aprendizagem, alunos que não possuem competências transversais; p. ex. competências sociais, verbais ou de estudo.',
+    options: [
+      'Estes dados não estão disponíveis e/ou não é minha responsabilidade analisá-los',
+      'Em parte, apenas analiso dados academicamente relevantes; p. ex. desempenho e classificações',
+      'Também levo em consideração dados sobre a atividade e o comportamento dos alunos, para identificar aqueles que precisam de apoio adicional',
+      'Examino regularmente toda a evidência disponível para identificar alunos que precisam de apoio adicional',
+      'Analiso dados sistematicamente e intervenho nos momentos adequados'
+    ],
+    selected: null
+  },
+  {
+    id: 3,
+    area: {
+      id: 4,
+      name: 'Avaliação'
+    },
+    question: 'Uso tecnologias digitais para fornecer feedback',
+    options: [
+      'O feedback não é necessário no meu contexto de trabalho',
+      'Forneço feedback aos alunos, mas não em formato digital',
+      'Às vezes utilizo formas digitais de prestar feedback; p. ex. pontuação automática em quizzes online ou “Curtidas” em ambientes digitais',
+      'Uso uma variedade de formas digitais de fornecer feedback',
+      'Uso sistematicamente abordagens digitais para fornecer feedback'
+    ],
+    selected: null
+  },
+  {
+    id: 1,
+    area: {
+      id: 5,
+      name: 'Capacitação dos Alunos'
+    },
+    question: 'Quando crio atividades digitais para os alunos, considero e abordo potenciais dificuldades práticas ou técnicas; p. ex., acesso equitativo a dispositivos e recursos digitais, problemas de interoperabilidade e conversão, falta de habilidades digitais, ...',
+    options: [
+      'Não crio tarefas digitais',
+      'Os meus alunos não têm problemas em utilizar tecnologia digital',
+      'Adapto a tarefa para minimizar dificuldades',
+      'Discuto possíveis obstáculos com os alunos e delineio soluções',
+      'Dou espaço para a variedade; p. ex. adapto a tarefa, discuto soluções e proporciono caminhos alternativos para completar a tarefa'
+    ],
+    selected: null
+  },
+  {
+    id: 2,
+    area: {
+      id: 5,
+      name: 'Capacitação dos Alunos'
+    },
+    question: 'Uso tecnologias digitais para proporcionar aos alunos oportunidades de aprendizagem personalizadas; p. ex., dou a diferentes alunos diferentes tarefas digitais para atender a necessidades individuais de aprendizagem, preferências e interesses',
+    options: [
+      'No meu contexto de trabalho, pede-se a todos os alunos que façam as mesmas atividades, independentemente do seu nível',
+      'Forneço aos alunos recomendações de recursos adicionais',
+      'Ofereço atividades digitais opcionais para os alunos que estão avançados ou atrasados',
+      'Sempre que possível, utilizo tecnologias digitais para oferecer oportunidades de aprendizagem diferenciadas',
+      'Adapto sistematicamente minhas aulas a relação com necessidades, preferências e interesses dos alunos'
+    ],
+    selected: null
+  },
+  {
+    id: 3,
+    area: {
+      id: 5,
+      name: 'Capacitação dos Alunos'
+    },
+    question: 'Uso tecnologias digitais para os alunos participarem ativamente nas aulas',
+    options: [
+      'No meu contexto de trabalho não é possível envolver os alunos ativamente na aula',
+      'Envolvo ativamente os alunos na aula, mas não com tecnologias digitais',
+      'Quando ensino, uso estímulos motivadores; p. ex. vídeos, animações',
+      'Os meus alunos envolvem-se com mídias digitais nas minhas aulas; p. ex. fichas de trabalho digitais, jogos, quizzes',
+      'Os meus alunos usam tecnologias digitais para investigar, discutir e criar conhecimento de forma sistemática'
+    ],
+    selected: null
+  },
+  {
+    id: 1,
+    area: {
+      id: 6,
+      name: 'Promoção da competências digital dos aprendentes'
+    },
+    question: 'Ensino aos meus alunos como avaliar a confiabilidade da informação, identificar desinformação e informação tendenciosa',
+    options: [
+      'Isto não é possível na minha disciplina ou contexto de trabalho',
+      'Ocasionalmente relembro aos alunos que nem toda a informação online é confiável',
+      'Ensino aos alunos como discernir fontes confiáveis e não confiáveis',
+      'Discuto com os alunos como verificar a precisão da informação',
+      'Discutimos, amplamente, como a informação é criada e pode ser distorcida'
+    ],
+    selected: null
+  },
+  {
+    id: 2,
+    area: {
+      id: 6,
+      name: 'Promoção da competências digital dos aprendentes'
+    },
+    question: 'Preparo tarefas que requerem que os alunos usem meios digitais para comunicarem e colaborarem uns com os outros ou com um público externo',
+    options: [
+      'Isto não é possível na minha disciplina ou contexto de trabalho',
+      'Apenas em raras ocasiões exijo aos meus alunos que comuniquem ou colaborem online',
+      'Os meus alunos usam comunicação e colaboração digital, sobretudo entre eles',
+      'Os meus alunos usam meios digitais para comunicarem e colaborarem entre eles e com um público externo',
+      'Preparo, sistematicamente, tarefas que permitem aos alunos expandirem lentamente as suas habilidades'
+    ],
+    selected: null
+  },
+  {
+    id: 3,
+    area: {
+      id: 6,
+      name: 'Promoção da competências digital dos aprendentes'
+    },
+    question: 'Preparo atividades que requerem que os alunos criem conteúdo digital; p. ex. vídeos, áudios, fotos, apresentações digitais, blogues, wikis ...',
+    options: [
+      'Isto não é possível na minha disciplina ou contexto de trabalho',
+      'Isto é difícil de implementar com os meus alunos',
+      'Às vezes, como uma atividade lúdica',
+      'Os meus alunos criam conteúdo digital como parte integrante do seu estudo',
+      'Isto é uma parte integrante da sua aprendizagem e eu aumento, sistematicamente, o nível de dificuldade para desenvolver ainda mais as suas habilidades'
+    ],
+    selected: null
+  },
+  {
+    id: 4,
+    area: {
+      id: 6,
+      name: 'Promoção da competências digital dos aprendentes'
+    },
+    question: 'Ensino os alunos a usarem tecnologia digital de forma segura e responsável',
+    options: [
+      'Isto não é possível na minha disciplina ou contexto de trabalho',
+      'Informo os alunos de que precisam de ter cuidado com o compartilhamento de informação pessoal online',
+      'Explico as regras básicas para agir com segurança e responsabilidade em ambientes online',
+      'Discutimos e estabelecemos regras de conduta',
+      'Desenvolvo, sistematicamente, a utilização de regras sociais nos diferentes ambientes digitais que utilizamos'
+    ],
+    selected: null
+  },
+  {
+    id: 5,
+    area: {
+      id: 6,
+      name: 'Promoção da competências digital dos aprendentes'
+    },
+    question: ' Incentivo os alunos a usarem tecnologias digitais de forma criativa para resolverem problemas concretos, p.ex., para superar obstáculos ou desafios emergentes no processo de aprendizagem',
+    options: [
+      'Isto não é possível na minha disciplina ou contexto de trabalho',
+      'Raramente tenho a oportunidade de promover a resolução de problemas digitais dos alunos',
+      'Ocasionalmente, quando surge uma oportunidade',
+      'Experimentamos, muitas vezes, soluções tecnológicas para problemas',
+      'Integro, sistematicamente, oportunidades para resolução criativa de problemas digitais'
+    ],
+    selected: null
+  },
 ])
 
 export const quizCompleted = ref(false);
--- src/pages/Chart.vue
+++ src/pages/Chart.vue
@@ -1,39 +1,90 @@
 <template>
   <div class="chart">
-    <Sidebar/>
+    <Sidebar />
     <div class="content">
       <h1 class="title-header">Gráficos</h1>
-      <span>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Asperiores deserunt tempora eaque assumenda obcaecati impedit amet
-        magnam, animi magni quam quo corrupti ab maiores qui quia provident 
-        dolore ut quis?
-      </span>
+      <div class="view-chart">
+        <canvas id="myChart" width="400" height="150"></canvas>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Chart from "chart.js/auto";
+
 export default {
-  name: 'Chart',
+  name: "Chart",
   data() {
     return {
-      areas: {}
-    }
+      areas: {},
+    };
   },
   mounted() {
     this.getValueAreas();
+    const ctx = document.getElementById("myChart");
+    const myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: [
+          "Envolvimento Profissional", 
+          "Recursos Digitais", 
+          "Ensino e Aprendizagem", 
+          "Avaliação", 
+          "Capacitação", 
+          "Promoção"
+        ],
+        datasets: [
+          {
+            label: "Nível De Competência Digital",
+            data: [
+              this.areas[0].result, 
+              this.areas[1].result, 
+              this.areas[2].result, 
+              this.areas[3].result, 
+              this.areas[4].result, 
+              this.areas[5].result
+            ],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    myChart;
   },
   methods: {
     getValueAreas() {
       let data = {};
-      let areasStringLocalStorage = localStorage.getItem('areas');
+      let areasStringLocalStorage = localStorage.getItem("areas");
       data = JSON.parse(areasStringLocalStorage);
       this.areas = data;
     },
     showResult() {
-      console.log(this.areas[0].result);      
-    }
-  }
-}
+      console.log(this.areas[0].result);
+    },
+  },
+};
 </script>
