56fcde2dffe117ce5a383d847c913981cd6a5c77
Pedro Rocha <pedrorocha73658@gmail.com>
2022-10-23 19:59:16 -0300

    Adding image in Home page and adjusting CSS it.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,23 +3,31 @@
     <Sidebar/>
     <div class="content">
       <div class="header">
-        <h1 class="header-title">Bem vindo</h1>
+        <h1 class="header-title">Bem vindo!</h1>
         <img src="../assets/images/selfie_logo.png" alt="Logomarca">
       </div>
-      <div class="text-body">
+      <div class="text-header">
+        <div class="text-body-image">
+          <img src="../assets/images/background.png" alt="Logo DigCompEdu">
+        </div>
         <p class="introduction">
           O Quadro Europeu de Competência Digital para Educadores responde à consciencialização 
           crescente entre muitos estados membros europeus que os educadores precisam de um 
           conjunto de competências digitais específicas para a sua profissão de modo a serem capazes 
           de aproveitar o potencial das tecnologias digitais para melhorar e inovar a educação.
         </p>
+      </div>
+      <div class="text-body">
+        <div class="digcompedu">
+          <h2>DIGCOMPEDU</h2>
+        </div>
+        <div class="areas-introduction">
+          <p class="areas-presentation">
+            O Quadro DigCompEdu visa captar e descrever estas competências digitais específicas, propondo 22 
+            competências elementares, organizadas em 6 áreas.
+          </p>
+        </div>
         <div class="areas">
-          <div class="areas-introduction">
-            <p class="areas-presentation">
-              O Quadro DigCompEdu visa captar e descrever estas competências digitais específicas, propondo 22 
-              competências elementares, organizadas em 6 áreas.
-            </p>
-          </div>
           <div class="areas-card" style="border: 2px solid #ff6384;">
             <img src="../assets/images/envolvimento.png" alt="Envolvimento Profissional">
             <p class="areas-presentation">
