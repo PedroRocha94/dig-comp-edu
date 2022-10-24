834bc7dceef7cbfdc468d5b9f9b9824829636a70
Pedro Rocha <pedrorocha73658@gmail.com>
2022-10-24 12:28:33 -0300

    Adding system description on Home page and tweaking CSS.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -17,6 +17,20 @@
           de aproveitar o potencial das tecnologias digitais para melhorar e inovar a educação.
         </p>
       </div>
+      <div class="info-about">
+        <div class="info-about-text">
+          <p class="info-about-text-p">
+            Este site tem como objetivo proporcionar ao usuário uma alternativa de auto avaliação em relação as suas 
+            competencias digitais. O uso do site é voltado para os professores, mas qualquer pessoa pode realizar sua
+            auto avaliação mas, tendo em mente que as pontuações podem ficar abaixo em algumas áreas específicas.
+          </p>
+          <p>
+            <strong>Competência digital</strong> são conhecimentos e criatividades que o sujeito tem para utilizar mídias digitais, 
+            tanto para o aprendizagem quanto para o uso em sociedade.
+          </p>
+        </div>
+        <img src="../assets/images/competencias.png" alt="Competencias">
+      </div>
       <div class="text-body">
         <div class="digcompedu">
           <h2>DIGCOMPEDU</h2>
@@ -31,7 +45,7 @@
           <div class="areas-card" style="border: 2px solid #ff6384;">
             <img src="../assets/images/envolvimento.png" alt="Envolvimento Profissional">
             <p class="areas-presentation">
-              A Área 1 dirige-se ao ambiente profissional, ou seja, ao uso de tecnologias digitais 
+              A Área 1. <strong>Envolvimento Profissional</strong>, dirige-se ao ambiente profissional, ou seja, ao uso de tecnologias digitais 
               por parte dos educadores em interações profissionais com colegas, aprendentes, para o 
               seu próprio desenvolvimento profissional e para o bem coletivo da instituição.
             </p>
@@ -39,34 +53,34 @@
           <div class="areas-card" style="border: 2px solid #36a2eb;">
             <img src="../assets/images/recursos.png" alt="Recurso">
             <p class="areas-presentation">
-              A Área 2 centra-se nas competências necessárias para usar, criar e partilhar recursos digitais para a aprendizagem,
+              A Área 2. <strong>Recurso</strong>, centra-se nas competências necessárias para usar, criar e partilhar recursos digitais para a aprendizagem,
               de forma efetiva e responsável.
             </p>
           </div>
           <div class="areas-card" style="border: 2px solid #ffce56;">
             <img src="../assets/images/ensino.png" alt="Ensino e Aprendizagem">
             <p class="areas-presentation">
-              A Área 3 é dedicada à gestão e orquestração da utilização de tecnologias digitais no ensino e aprendizagem.
+              A Área 3. <strong>Ensino e Aprendizagem</strong>, é dedicada à gestão e orquestração da utilização de tecnologias digitais no ensino e aprendizagem.
             </p>
           </div>
           <div class="areas-card" style="border: 2px solid #4bc0c0;">
             <img src="../assets/images/avaliacao.png" alt="Avaliação">
             <p class="areas-presentation">
-              A Área 4 aborda o uso de estratégias digitais para melhorar a avaliação.
+              A Área 4. <strong>Avaliação</strong>, aborda o uso de estratégias digitais para melhorar a avaliação.
             </p>
           </div>
           <div class="areas-card" style="border: 2px solid #9966ff;">
             <img src="../assets/images/capacitacao.png" alt="Capacitação dos alunos">
             <p class="areas-presentation">
-              A Área 5 concentra-se no potencial das tecnologias digitais para estratégias de ensino e aprendizagem centradas
-              no aprendente.
+              A Área 5. <strong>Capacitação dos Alunos</strong>, concentra-se no potencial das tecnologias digitais para estratégias de ensino e aprendizagem centradas
+              no aluno.
             </p>
           </div>
           <div class="areas-card" style="border: 2px solid #ff9f40;">
             <img src="../assets/images/promocao.png" alt="Promoção">
             <p class="areas-presentation">
-              Área 6 detalha as competências pedagógicas específicas necessárias para promover a competência digital dos 
-              aprendentes.
+              A Área 6. <strong>Promoção da Competências Digital dos Alunos</strong>, detalha as competências pedagógicas específicas necessárias para promover a competência digital dos 
+              alunos.
             </p>
           </div>
         </div>
